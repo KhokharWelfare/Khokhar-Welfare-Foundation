b59e9a6baeeafacdc4a3532faa3252d29a638415
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-06-02 21:30:06 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 const cors = require('cors');
 app.use(cors({
-  origin: 'https://khokhar-welfarefoundation.vercel.app',
+  origin: ['https://khokhar-welfarefoundation.vercel.app', 'https://www.khokharwelfarefoundaion.com'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
