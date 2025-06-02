e52afcaa7bcc367fe519366970a4bb3c3fa4626e
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-06-02 21:30:55 +0500

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div>
-        <p>Contact us: khokharwelfare2024@gmail.com | +92 301 7664983  |  +92 320 7134010</p>
+        <p>Contact us: khokharwelfare2024@gmail.com | +92 301 7664983  |  +92 320 7134010  |  +92 306 9095996</p>
         <p>Office Address: Ashab e Suffa Islamic Center, Multan, Pakistan</p>
         <p>© 2024 Khokhar Welfare Foundation. All rights reserved.</p>
       </div>
