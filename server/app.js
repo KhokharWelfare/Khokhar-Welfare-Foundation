5f316c25fc6e7d8679d4e4ec65192ece441e8edf
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-06-11 10:51:43 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -81,5 +81,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
